657018724b26ecb2a0a596afbcbece2159832e79
Jérémie Boulay <jboulay@prox-04018x.bbdo.org>
2016-11-03 16:15:48 +0100

    :gem: perspective paysage with frog

--- 2016/03_Volcano/app.js
+++ 2016/03_Volcano/app.js
@@ -1,6 +1,5 @@
 import React, { Component, PropTypes } from 'react';
 import ReactDOM from 'react-dom';
-import dat from 'dat-gui';
 
 import SquigglySVG from '../00_modules/squiggly/SquigglySVG.js';
 
