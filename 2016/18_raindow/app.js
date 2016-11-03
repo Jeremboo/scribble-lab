218fdde5bff08d66d5e5bbc7d15531cb0449e3f9
Jérémie Boulay <jboulay@prox-04018x.bbdo.org>
2016-11-03 14:07:00 +0100

    :tada: raindow sphere

---
+++ 2016/18_raindow/app.js
@@ -0,0 +1,97 @@
+import threeJs from 'three-js';
+
+const THREE = threeJs();
+
+/**/ /* ---- CORE ---- */
+/**/ const mainColor = '#070707';
+/**/ const secondaryColor = '#C9F0FF';
+/**/ const bgColor = '#070707';
+/**/ let windowWidth = window.innerWidth;
+/**/ let windowHeight = window.innerHeight;
+/**/ class Webgl {
+/**/   constructor(w, h) {
+/**/     this.meshCount = 0;
+/**/     this.meshListeners = [];
+/**/     this.renderer = new THREE.WebGLRenderer({ antialias: true });
+/**/     this.renderer.setPixelRatio(window.devicePixelRatio);
+/**/     this.renderer.setClearColor(new THREE.Color(bgColor));
+/**/     this.scene = new THREE.Scene();
+/**/     this.camera = new THREE.PerspectiveCamera(50, w / h, 1, 1000);
+/**/     this.camera.position.set(0, 0, 10);
+/**/     this.dom = this.renderer.domElement;
+/**/     this.update = this.update.bind(this);
+/**/     this.resize = this.resize.bind(this);
+/**/     this.resize(w, h); // set render size
+/**/   }
+/**/   add(mesh) {
+/**/     this.scene.add(mesh);
+/**/     if (!mesh.update) return;
+/**/     this.meshListeners.push(mesh.update);
+/**/     this.meshCount++;
+/**/   }
+/**/   update() {
+/**/     let i = this.meshCount;
+/**/     while (--i >= 0) {
+/**/       this.meshListeners[i].apply(this, null);
+/**/     }
+/**/     this.renderer.render(this.scene, this.camera);
+/**/   }
+/**/   resize(w, h) {
+/**/     this.camera.aspect = w / h;
+/**/     this.camera.updateProjectionMatrix();
+/**/     this.renderer.setSize(w, h);
+/**/   }
+/**/ }
+/**/ const webgl = new Webgl(windowWidth, windowHeight);
+/**/ document.body.appendChild(webgl.dom);
+/**/
+/**/
+/* ---- CREATING ZONE ---- */
+
+import img from '../00_inspirations/rainbow.png';
+
+// // OBJECTS
+class Panorama extends THREE.Object3D {
+  constructor(img, depth) {
+    super();
+
+    this.material = new THREE.MeshBasicMaterial({
+      map: THREE.ImageUtils.loadTexture(img),
+    });
+    this.geometry = new THREE.SphereGeometry(depth, 100, 40);
+    this.geometry.applyMatrix(new THREE.Matrix4().makeScale(-1, 1, 1));
+    this.mesh = new THREE.Mesh(this.geometry, this.material);
+
+    this.add(this.mesh);
+
+    this.update = this.update.bind(this);
+  }
+
+  update() {
+    this.rotation.y += 0.05;
+  }
+}
+
+// START
+// TODO use canvas to texture
+const pano1 = new Panorama(img, 1.5);
+webgl.add(pano1);
+
+
+/* ---- CREATING ZONE END ---- */
+/**/
+/**/
+/**/ /* ---- ON RESIZE ---- */
+/**/ function onResize() {
+/**/   windowWidth = window.innerWidth;
+/**/   windowHeight = window.innerHeight;
+/**/   webgl.resize(windowWidth, windowHeight);
+/**/ }
+/**/ window.addEventListener('resize', onResize);
+/**/ window.addEventListener('orientationchange', onResize);
+/**/ /* ---- LOOP ---- */
+/**/ function _loop(){
+/**/ 	webgl.update();
+/**/ 	requestAnimationFrame(_loop);
+/**/ }
+/**/ _loop();
