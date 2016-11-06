0776d511efb8404db8cb986e63d6b9c0372c1e81
Jérémie Boulay <jeremi.boulay@gmail.com>
2016-11-07 00:32:27 +0100

    :gem: random style in JS

--- 2016/06_childrenCubes/app.js
+++ 2016/06_childrenCubes/app.js
@@ -1,17 +1,46 @@
-const STYLES = [
-  'basic',
-  'new',
+const faces = document.getElementsByClassName('side');
+const COLORS = [
+  '#f4f3d2',
+  '#F1515C',
+  '#22D4BE',
+  '#433D3A',
+];
+const BORDER_STYLE = [
+  'solid',
+  'dashed',
 ];
 
-const faces = document.getElementsByClassName('side');
-
-let styleId = 0;
+const getRandomInt = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
+const removeElementFromArray = (element, array) => {
+  const arr = [...array];
+  const index = arr.indexOf(element)
+  if (index > -1) arr.splice(index, 1);
+  return arr;
+}
+const getRandomElm = (arr = COLORS) => arr[getRandomInt(0, arr.length - 1)];
 window.setInterval(() => {
-  const oldStyle = STYLES[styleId];
-  styleId = (styleId + 1) % STYLES.length;
   let i;
+  let backgroundColor = getRandomElm();
+  let newColorArray = removeElementFromArray(backgroundColor, COLORS);
+  let fontColor = getRandomElm(newColorArray);
+  const borderTextColor = getRandomElm();
+  const randomColors = Math.random() > 0.6;
+  const border = {
+    width: `${getRandomInt(0, 3)}px`,
+    style: getRandomElm(BORDER_STYLE),
+    color: getRandomElm(),
+  };
   for (i = 0; i < faces.length; i++) {
-    faces[i].classList.remove(oldStyle);
-    faces[i].classList.add(STYLES[styleId]);
+    if (randomColors) {
+      backgroundColor = getRandomElm();
+      newColorArray = removeElementFromArray(backgroundColor, COLORS);
+      fontColor = getRandomElm(newColorArray);
+    }
+    faces[i].style.color = fontColor;
+    faces[i].style.backgroundColor = backgroundColor;
+    faces[i].style.borderColor = border.color;
+    faces[i].style.borderWidth = border.width;
+    faces[i].style.borderStyle = border.style;
+    faces[i].style.textShadow = `1px 0 0 ${borderTextColor}, -(1px) 0 0 ${borderTextColor}, 0 1px 0 ${borderTextColor}, 0 -(1px) 0 ${borderTextColor}, 1px 1px ${borderTextColor}, -1px -1px 0 ${borderTextColor}, 1px -1px 0 ${borderTextColor}, -1px 1px 0 ${borderTextColor}`;
   }
-}, 1000);
+}, 150);
