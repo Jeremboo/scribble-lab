96e106efa2c7726f0ddf81ac3dce928d335c9401
Jérémie Boulay <boulayj@proximity.bbdo.fr>
2017-05-31 15:11:09 +0200

    :gem: :books: add params everyhere

--- scribbles/webglparticles/2DStorm/app.js
+++ scribbles/webglparticles/2DStorm/app.js
@@ -12,103 +12,105 @@
 /**/   windowHeight = canvas.height = window.innerHeight;
 /**/ };
 /**/ /* ---- CORE END ---- */
+
+
 /* ---- CREATING ZONE ---- */
 
-
 /* ---- SETTINGS ---- */
-const PARTICLE_NUMBERS = 1000;
-const PARTICULE_AMPL = 0.4;
-const FORCE_VELOCITY = 0.002;
-const VEL_MAX = 0.001;
-const VELOCITY = 0.95;
+const PARTICLE_NUMBERS = 100;
+
+const VEL_MAX = 5;
+const VEL_MIN = 0;
+const VEL_BRAKE_MIN = 0.9;
+const VEL_BRAKE_MAX = 0.95;
+
+const LINE_WIDTH = 5;
+
+const DEMISE_DISTANCE = 10;
+
+const ATTRACTIVE_AMPL = 3.8; // To reduce the force of the attraction at center
+const ATTRACTIVE_ZONE = 0.1;
+const ATTRACTIVE_FORCE = 0.02;
+
+const ROTATION_FORCE = 10;
 
 const COLORS = ['#32234A', '#820263', '#D90368', '#883677', '#CA61C3'];
 
 Math.sqr = (a) => a * a;
 Math.randomF = (min, max) => Math.random() * (max - min) + min;
+const getDist = (x1, y1, x2, y2) => Math.sqrt(Math.sqr(y2 - y1) + Math.sqr(x2 - x1));
+const getRandomAngle = () => Math.random() * Math.PI * 2;
 
-function getDist(x1, y1, x2, y2) {
-  return Math.sqrt(Math.sqr(y2 - y1) + Math.sqr(x2 - x1));
-}
-
-/* ---- Particle ---- */
+/* ---- PARTICLE ---- */
 class Particle {
   constructor(x, y) {
-    this.x = x;
-    this.y = y;
-
-    this.vel = Math.randomF(-10, 10);
-    this.vel = this.getRotationForce();
-
-    this.vel = {
-      x: 0,
-      y: 0,
-      max: Math.randomF(VEL_MAX - 1, VEL_MAX),
-    };
-
-    this.train = [];
-
-    // temps
-    // this.train.push({ x: this.x, y: this.y });
-    // this.train.push({ x: this.x, y: this.y });
-
+    this.init(x, y);
     this.color = COLORS[Math.floor(Math.random() * COLORS.length)];
   }
 
-  initRandomPosition() {
-    const angle = Math.random() * Math.PI * 2;
-    const radius = Math.randomF(0, 550);
-    this.x = Math.cos(angle) * radius;
-    this.y = Math.sin(angle) * radius;
+  init(x, y) {
+    const angle = getRandomAngle();
+    const radius = Math.randomF(0, windowWidth);
+    this.x = x || Math.cos(angle) * radius;
+    this.y = y || Math.sin(angle) * radius;
     this.train = [];
+    this.vel = {
+      x: 0,
+      y: 0,
+      max: Math.randomF(VEL_MIN, VEL_MAX),
+      brake: Math.randomF(VEL_BRAKE_MIN, VEL_BRAKE_MAX),
+    };
   }
 
   render() {
     context.beginPath();
     context.strokeStyle = this.color;
-    context.lineWidth = 1;
+    context.lineWidth = LINE_WIDTH; // TODO augmenter la taille du trai en fonction de la distance ?
 
-    // Show the vel vector
-    // context.arc(this.x, this.y, 1, 0, 2 * Math.PI, false);
-    //const force = this.getRotationForce();
-    // context.moveTo(this.x, this.y);
-    //context.lineTo(force.x + this.x, force.y + this.y);
-
-    context.moveTo(this.train[0].x,this.train[0].y);
     let i = this.train.length - 1;
     for (i; i > 0; i--) {
-      context.lineTo(this.train[i].x,this.train[i].y);
+      context.lineTo(this.train[i].x, this.train[i].y);
     }
     context.stroke();
-
-    // TODO if it is near the center, init at a random position
-    const dist = getDist(this.x, this.y, 0, 0);
-    if (dist < 30) {
-      this.initRandomPosition();
-    }
   }
 
   update() {
-    // Get force
-    const force = {
-      x: this.x * FORCE_VELOCITY,
-      y: this.y * FORCE_VELOCITY,
+    // init values
+    const dist = getDist(this.x, this.y, 0, 0);
+
+    // Init a Particle when he near the center
+    if (dist < DEMISE_DISTANCE) {
+      this.init();
+      return;
+    }
+
+    const norm = {
+      x: this.x / dist,
+      y: this.y / dist,
     };
 
+    // calculate force
+    const f = Math.exp(ATTRACTIVE_AMPL * (1 - (dist / windowWidth) - ATTRACTIVE_ZONE)) * ATTRACTIVE_FORCE;
+    const force = {
+      x: norm.x * f,
+      y: norm.y * f,
+    };
+
+    // update velocity
     this.vel.x += force.x;
     this.vel.y += force.y;
 
-    this.x -= this.vel.x * PARTICULE_AMPL;
-    this.y -= this.vel.y * PARTICULE_AMPL;
+    if (Math.abs(this.vel.x) > this.vel.max) this.vel.x *= this.vel.brake;
+    if (Math.abs(this.vel.y) > this.vel.max) this.vel.y *= this.vel.brake;
 
-    const f = this.getRotationForce();
-    this.x += (f.x * 0.3);
-    this.y += (f.y * 0.3);
+    // Apply attraction
+    this.x -= this.vel.x;
+    this.y -= this.vel.y;
 
-    if(Math.abs(this.vel.x) > this.vel.max)
-        this.vel.x *= VELOCITY;
-    if(Math.abs(this.vel.y) > this.vel.max)
-        this.vel.y *= VELOCITY;
+    // Apply rotation
+    const rf = this.getRotationForce();
+    this.x += (rf.x * f);
+    this.y += (rf.y * f);
 
     // Trains
     this.train.push({
@@ -130,35 +132,25 @@ class Particle {
       y: orthogonalVec.y / dist,
     };
 
-    // Get force
-    const distFromOrigin = getDist(0, 0, this.x, this.y);
-    const f = Math.exp(2) + (distFromOrigin * 0.02);
-
     return {
-      x: normalizedOrthogonalVec.x * f,
-      y: normalizedOrthogonalVec.y * f,
+      x: normalizedOrthogonalVec.x * ROTATION_FORCE,
+      y: normalizedOrthogonalVec.y * ROTATION_FORCE,
     };
   }
 }
 
 // START
-
 const particles = [];
-
 for (let i = 0; i < PARTICLE_NUMBERS; i++) {
-  const angle = Math.random() * Math.PI * 2;
-  const radius = Math.randomF(0, 550);
-	particles.push(new Particle(
-		Math.cos(angle) * radius,
-		Math.sin(angle) * radius,
-	));
+  particles.push(new Particle());
 }
 
+let i;
 function loop() {
-  particles.map((p, index) => {
-    p.update();
-    p.render();
-	});
+  for (i = 0; i < particles.length; i++) {
+    particles[i].update();
+    particles[i].render();
+  }
 }
 
 
@@ -169,5 +161,11 @@ function loop() {
 /**/   loop();
 /**/ 	 requestAnimationFrame(_loop);
 /**/ }
+/**/ window.onresize = () => {
+/**/   windowWidth = canvas.width = window.innerWidth;
+/**/   windowHeight = canvas.height = window.innerHeight;
+/**/   origin = { x: windowWidth * 0.5, y: windowHeight * 0.5 };
+/**/   context.translate(origin.x, origin.y);
+/**/ };
 /**/ _loop();
 /**/ /* ---- LOOP END ---- */
