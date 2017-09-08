c33c1bdd8eb23dd8a59c7ccde0d9a341441456be
Jérémie Boulay <boulayj@proximity.bbdo.fr>
2017-09-08 17:17:49 +0200

    :arrow_up: udpate webpack and some packages

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,9 @@ var webpack = require('webpack');
 var poststylus = require('poststylus');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 
-var myLocalIp = 'http://' + ip.address() + ':3333/';
+var port = 3333;
+var ipAdress = ip.address() + ':' + port;
+var myLocalIp = 'http://' + ipAdress + '/';
 
 // DIRECTORIES
 var assets = path.resolve(__dirname, process.env.GROUP_PATH + '/_assets/');
@@ -18,16 +20,26 @@ var node_modules = path.resolve(__dirname, './node_modules');
 var config = {
     entry: [
       'babel-polyfill',
-      'webpack/hot/dev-server',
-      'webpack-dev-server/client?' + myLocalIp,
       path.resolve(__dirname, './bootstrap.js')
     ],
     output: {
         path: path.resolve(__dirname, scribble_path + '/'),
         filename: 'bundle.js',
     },
-    debug: true,
-    devtool: 'eval-source-map',
+    devtool: "eval-source-map",
+    devServer: {
+      // compress: true,
+      contentBase: path.resolve(__dirname, '/'),
+      headers: { 'Access-Control-Allow-Origin': '*' },
+      historyApiFallback: true,
+      disableHostCheck: true,
+      host: '0.0.0.0',
+      hot: true,
+      inline: true,
+      port: port,
+      // Release of webpack-dev-server 2.4.3 => https://github.com/webpack/webpack-dev-server/issues/882
+      public: ipAdress,
+    },
     resolve: {
       alias: {
         html: path.resolve(__dirname, scribble_path + '/index.pug.html'),
@@ -36,12 +48,11 @@ var config = {
       },
     },
     module: {
-      noParse: [],
-      loaders: [
+      rules: [
         {
           test: /\.jsx?$/,
           exclude: node_modules,
-          loader: 'babel',
+          loader: 'babel-loader',
           query: {
             plugins: [
               [ 'module-resolver', {
@@ -52,41 +63,51 @@ var config = {
         },
         {
           test: /\.(styl|css)$/,
-          loader: 'style!css!stylus',
+          use: [
+            'style-loader',
+            {
+              loader: 'css-loader',
+              options: { sourceMap: true },
+            },
+            {
+              loader: 'stylus-loader',
+              options: { sourceMap: true },
+            },
+          ],
         },
         {
           test: /\.(png|jpe?g|gif)$/,
-          loader: 'file?name=imgs/[hash].[ext]',
+          loader: 'file-loader?name=imgs/[hash].[ext]',
           include: [ './assets/', assets ],
           exclude: [ './assets/raw/', assets + 'raw/' ],
         },
         {
           test: /\.(svg)$/,
-          loader: 'raw',
+          loader: 'raw-loader',
           include: [ './assets/raw/', assets + 'raw/' ],
         },
         {
           test: /\.(html|pug)$/,
-          loader: 'pug',
+          loader: 'pug-loader',
         },
         {
           test: /\.(eot|svg|ttf|woff(2)?)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-          loader: 'file?name=fonts/[name].[ext]',
+          loader: 'file-loader?name=fonts/[name].[ext]',
           include: [ './assets/', assets ],
         },
-        { test: /\.(glsl|frag|vert)$/, exclude: node_modules, loader: 'raw' },
-        { test: /\.(glsl|frag|vert)$/, exclude: node_modules, loader: 'glslify' },
+        { test: /\.(glsl|frag|vert)$/, exclude: node_modules, loader: 'raw-loader' },
+        { test: /\.(glsl|frag|vert)$/, exclude: node_modules, loader: 'glslify-loader' },
       ],
     },
-    stylus: {
-      use: [
-        poststylus(['autoprefixer'])
-      ]
-    },
     plugins: [
-      new webpack.optimize.OccurenceOrderPlugin(),
-      new webpack.HotModuleReplacementPlugin(),
-      new webpack.optimize.DedupePlugin(),
+      new webpack.LoaderOptionsPlugin({
+        debug: true,
+        options: {
+          stylus: {
+            use: [poststylus(['autoprefixer'])],
+          },
+        },
+      }),
       new HtmlWebpackPlugin({
         title: name,
         template: path.resolve(__dirname, './index.html'),
