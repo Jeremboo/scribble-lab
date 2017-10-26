31cfba34da7fc6debca1831451388e40f1b20525
Jérémie Boulay <jeremi.boulay@gmail.com>
2017-10-26 08:47:42 +0200

    :gem: new pen idea and debug package.json

---
+++ scribbles/codevemenr2017/shuffle/app.js
@@ -0,0 +1,3 @@
+// Random text shuffle: https://codepen.io/SaschaSigl/pen/woGYKJ?q=shuffle&order=popularity&depth=everything&show_forks=false
+// Tuto shuffle letters effect: https://tutorialzine.com/2011/09/shuffle-letters-effect-jquery
+// Check wave: http://lab.hakim.se/checkwave/
