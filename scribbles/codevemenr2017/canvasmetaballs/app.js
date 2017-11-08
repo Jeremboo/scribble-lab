b7339f6d6792c340587b155506d55c1b55c3e753
Jérémie Boulay <boulayj@proximity.bbdo.fr>
2017-11-08 16:03:33 +0100

    :gem: update metaballs

--- scribbles/codevemenr2017/canvasmetaballs/app.js
+++ scribbles/codevemenr2017/canvasmetaballs/app.js
@@ -1,5 +1,6 @@
-export const sqr = a => a * a;
-const getDistBetweenTwoVec2 = (x1, y1, x2, y2) => Math.sqrt(sqr(y2 - y1) + sqr(x2 - x1));
+Math.sqr = a => a * a;
+const getVec2Length = (x, y) => Math.sqrt(Math.sqr(y) + Math.sqr(x));
+const getDistBetweenTwoVec2 = (x1, y1, x2, y2) => getVec2Length(x2 - x1, y2 - y1);
 
 const canvasBuilder = (width = window.innerWidth, height = window.innerHeight) => {
   const canvas = document.createElement('canvas');
@@ -67,10 +68,9 @@ class BubbleCanvas {
   // Save state here
   constructor(size) {
     this.size = size;
-    this.bubbleSize = size * 0.5;
+    this.bubbleSize = size;
     this.littleBubbleSize = size * 0.5;
     this.threshold = 200;
-    this.backgroundImg = false;
 
     this.center = {
       x: this.size * 0.5,
@@ -87,40 +87,57 @@ class BubbleCanvas {
       y: this.size * 0.5,
     };
 
+    this.littleBubbleForce = {
+      x: 0,
+      y: 0,
+    };
+
     // Create canvas
     const { canvas, context } = canvasBuilder(this.size, this.size);
     this.canvas = canvas;
     this.context = context;
-
-    // Load background image
-    // https://i.imgur.com/462xXUs.png
-    // https://pixabay.com/get/eb3db40a2ef2073ed95c4518b74d4095e272e1dc04b014419cf9c97fafebb4_640.jpg
-    applyImageToCanvas('https://i.imgur.com/462xXUs.png', this.size, this.size).then((_canvas) => {
-      this.backgroundImg = _canvas;
-    });
-
     // getMousePosition
     canvas.addEventListener('mousemove', (e) => {
       this.mousePosition.x = e.offsetX;
       this.mousePosition.y = e.offsetY;
     });
     canvas.addEventListener('mouseout', () => {
-      this.mousePosition.x = this.size * 0.5;
-      this.mousePosition.y = this.size * 0.5;
+      this.mousePosition.x = this.center.x;
+      this.mousePosition.y = this.center.y;
+    });
+
+    // Load background image
+    // https://i.imgur.com/462xXUs.png
+    // https://pixabay.com/get/eb3db40a2ef2073ed95c4518b74d4095e272e1dc04b014419cf9c97fafebb4_640.jpg
+    this.backgroundImg = false;
+    applyImageToCanvas('https://i.imgur.com/462xXUs.png', this.size, this.size).then((_canvas) => {
+      this.backgroundImg = _canvas;
     });
   }
 
   update() {
-    // Update the positions of the little bubble
-    this.littleBubble.x += (this.mousePosition.x - this.littleBubble.x) * 0.1;
-    this.littleBubble.y += (this.mousePosition.y - this.littleBubble.y) * 0.1;
+    const vecForce = {
+      x: this.mousePosition.x - this.littleBubble.x,
+      y: this.mousePosition.y - this.littleBubble.y,
+    };
+    const force = Math.max(0, (this.size * 0.3) - getVec2Length(vecForce.x, vecForce.y));
 
+    this.littleBubbleForce.x += vecForce.x * force * 0.001;
+    this.littleBubbleForce.y += vecForce.y * force * 0.001;
+
+    this.littleBubble.x += this.littleBubbleForce.x;
+    this.littleBubble.y += this.littleBubbleForce.y;
+
+    this.littleBubble.x += (this.center.x - this.littleBubble.x) * 0.2;
+    this.littleBubble.y += (this.center.y - this.littleBubble.y) * 0.2;
+
+    this.littleBubbleForce.x *= 0.8;
+    this.littleBubbleForce.y *= 0.8;
 
     // play with the scale
     // const dist = getDistBetweenTwoVec2(this.littleBubble.x, this.littleBubble.y, this.center.x, this.center.y);
     // const scale = Math.min(Math.max(0, (5 - ((dist / this.size) * 20))), 1)
     // this.littleBubbleSize = this.bubbleSize * scale;
-
   }
 
   render() {
@@ -130,8 +147,8 @@ class BubbleCanvas {
       this.context.globalCompositeOperation = 'source-over';
 
       // Gradient circles
-      gradientArc(this.context, { x: this.center.x, y: this.center.y, size: this.bubbleSize * 2, ratio: 0.4 });
-      gradientArc(this.context, { x: this.littleBubble.x, y: this.littleBubble.y, size: this.littleBubbleSize, ratio: 0.1 });
+      gradientArc(this.context, { x: this.center.x, y: this.center.y, size: this.bubbleSize, ratio: 0.4 });
+      gradientArc(this.context, { x: this.littleBubble.x, y: this.littleBubble.y, size: this.littleBubbleSize, ratio: 0.2 });
 
       // threshold
       this.renderThershold();
@@ -142,14 +159,14 @@ class BubbleCanvas {
   }
 
   renderThershold() {
-    const imageData = this.context.getImageData(0, 0, this.size, this.size)
+    const imageData = this.context.getImageData(0, 0, this.size, this.size);
     const pix = imageData.data;
 
     for (let i = 3; i < pix.length; i += 4) {
       if (pix[i] < this.threshold) {
         pix[i] = 0;
       } else {
-        pix[i] = 255;
+        pix[i] = Math.min(255, (pix[i] - this.threshold) * 20);
       }
     }
     this.context.putImageData(imageData, 0, 0);
