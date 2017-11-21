e2d189ee7869098314cfd15776f64c27db619c89
Jérémie Boulay <jeremi.boulay@gmail.com>
2017-11-21 10:21:18 +0100

    :bug: fix build scribble :bug: add good dat-gui package

--- scripts/buildScribble.js
+++ scripts/buildScribble.js
@@ -12,10 +12,12 @@ config.entry = [
   'babel-polyfill',
   path.resolve(__dirname, '../bootstrap.js'),
 ];
-config.debug = false;
+// config.debug = false;
+config.devServer = {};
 config.devtool = '';
 config.output.path = path.resolve(__dirname, `../public/${dirs[1]}/${dirs[2]}`);
-config.module.loaders[1].loader = ExtractTextPlugin.extract('style', 'css?sourceMap!stylus');
+config.output.publicPath = '';
+config.plugins.splice(0, 1);
 config.plugins.push(new webpack.optimize.UglifyJsPlugin({
   compress: { warnings: true, drop_console: true },
   comments: false,
@@ -23,12 +25,16 @@ config.plugins.push(new webpack.optimize.UglifyJsPlugin({
   mangle: true,
   minimize: true,
 }));
-config.plugins.push(new ExtractTextPlugin('styles.css', {
-  disable: false,
-  allChunks: true,
-}));
+
+// TODO add extractTextPlugin
+// config.module.rules[1].loader = ExtractTextPlugin.extract('style', 'css?sourceMap!stylus');
+// config.plugins.push(new ExtractTextPlugin('styles.css', {
+//   disable: false,
+//   allChunks: true,
+// }));
 
 // Compile
+console.log(config)
 const compiler = webpack(config);
 compiler.run((err, stats) => {
   console.log(err);
