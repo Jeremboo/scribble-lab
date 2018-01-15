71534b9dfe3b32ff01e737381a49b6b3e2dbc5bf
Jérémie Boulay <boulayj@proximity.bbdo.fr>
2018-01-15 18:35:43 +0100

    :gem: add Raymarcher class by nicoptere :heart:

--- scribbles/blob/raymarching/app.js
+++ scribbles/blob/raymarching/app.js
@@ -3,51 +3,9 @@ import {
   MeshBasicMaterial, Mesh, Color, FlatShading,
 } from 'three';
 
-/**/ /* ---- CORE ---- */
-/**/ const mainColor = '#070707';
-/**/ const secondaryColor = '#C9F0FF';
-/**/ const bgColor = false // 'rgb(0, 0, 0)';
-/**/ let windowWidth = window.innerWidth;
-/**/ let windowHeight = window.innerHeight;
-/**/ class Webgl {
-/**/   constructor(w, h) {
-/**/     this.meshCount = 0;
-/**/     this.meshListeners = [];
-/**/     this.renderer = new WebGLRenderer({ antialias: true, alpha: true });
-/**/     this.renderer.setPixelRatio(window.devicePixelRatio);
-/**/     if (bgColor) this.renderer.setClearColor(new Color(bgColor));
-/**/     this.scene = new Scene();
-/**/     this.camera = new PerspectiveCamera(50, w / h, 1, 1000);
-/**/     this.camera.position.set(0, 0, 10);
-/**/     this.dom = this.renderer.domElement;
-/**/     this.update = this.update.bind(this);
-/**/     this.resize = this.resize.bind(this);
-/**/     this.resize(w, h); // set render size
-/**/   }
-/**/   add(mesh) {
-/**/     this.scene.add(mesh);
-/**/     if (!mesh.update) return;
-/**/     this.meshListeners.push(mesh.update);
-/**/     this.meshCount++;
-/**/   }
-/**/   update() {
-/**/     let i = this.meshCount;
-/**/     while (--i >= 0) {
-/**/       this.meshListeners[i].apply(this, null);
-/**/     }
-/**/     this.renderer.render(this.scene, this.camera);
-/**/   }
-/**/   resize(w, h) {
-/**/     this.camera.aspect = w / h;
-/**/     this.camera.updateProjectionMatrix();
-/**/     this.renderer.setSize(w, h);
-/**/   }
-/**/ }
-/**/ const webgl = new Webgl(windowWidth, windowHeight);
-/**/ document.body.appendChild(webgl.dom);
-/**/
-/**/
-/* ---- CREATING ZONE ---- */
+import rm from 'RayMarcher';
+
+import blobFrag from './shaders/blob.f.glsl';
 
 /**
  * DOCS
@@ -62,51 +20,29 @@ import {
  * - http://iquilezles.org/www/material/nvscene2008/rwwtt.pdf
  */
 
-
-// OBJECTS
-class Example extends Object3D {
-  constructor() {
-    super();
-
-    this.material = new MeshBasicMaterial({
-      color: new Color(secondaryColor),
-      shading: FlatShading,
-      wireframe: true,
-    });
-    this.geometry = new BoxGeometry(1, 1, 1);
-    this.mesh = new Mesh(this.geometry, this.material);
-
-    this.add(this.mesh);
-
-    this.update = this.update.bind(this);
-  }
-
-  update() {
-    this.rotation.x += 0.03;
-    this.rotation.y += 0.03;
-  }
+function animate() {
+  requestAnimationFrame(animate);
+  rm.update();
+  rm.render();
 }
 
-// START
-const ex = new Example();
+function onFragmentLoaded(scope) {
+  scope.setTexture("map", "img/matcap.png");
+  animate();
+}
 
-// ADDS
-webgl.add(ex);
+function init()  {
+  rm.setFragmentShader(blobFrag, onFragmentLoaded);
+  document.body.appendChild(rm.domElement);
 
-/* ---- CREATING ZONE END ---- */
-/**/
-/**/
-/**/ /* ---- ON RESIZE ---- */
-/**/ function onResize() {
-/**/   windowWidth = window.innerWidth;
-/**/   windowHeight = window.innerHeight;
-/**/   webgl.resize(windowWidth, windowHeight);
-/**/ }
-/**/ window.addEventListener('resize', onResize);
-/**/ window.addEventListener('orientationchange', onResize);
-/**/ /* ---- LOOP ---- */
-/**/ function _loop() {
-/**/ 	webgl.update();
-/**/ 	requestAnimationFrame(_loop);
-/**/ }
-/**/ _loop();
+  window.addEventListener("resize", onResize, false);
+  onResize();
+}
+
+
+
+function onResize(e) {
+  rm.setSize(window.innerWidth, window.innerHeight);
+}
+
+init();
