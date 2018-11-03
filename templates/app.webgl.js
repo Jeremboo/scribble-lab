686ce5c9aab8a0b9e6fffe942e811fe47d80628a
Jérémie Boulay <jeremi.boulay@gmail.com>
2018-11-02 21:20:17 -0400

    :art: update webgl template

--- templates/app.webgl.js
+++ templates/app.webgl.js
@@ -3,51 +3,59 @@ import {
   MeshBasicMaterial, Mesh, Color, FlatShading,
 } from 'three';
 
-/**/ /* ---- CORE ---- */
-/**/ const mainColor = '#070707';
-/**/ const secondaryColor = '#C9F0FF';
-/**/ const bgColor = false // 'rgb(0, 0, 0)';
-/**/ let windowWidth = window.innerWidth;
-/**/ let windowHeight = window.innerHeight;
-/**/ class Webgl {
-/**/   constructor(w, h) {
-/**/     this.meshCount = 0;
-/**/     this.meshListeners = [];
-/**/     this.renderer = new WebGLRenderer({ antialias: true, alpha: true });
-/**/     this.renderer.setPixelRatio(window.devicePixelRatio);
-/**/     if (bgColor) this.renderer.setClearColor(new Color(bgColor));
-/**/     this.scene = new Scene();
-/**/     this.camera = new PerspectiveCamera(50, w / h, 1, 1000);
-/**/     this.camera.position.set(0, 0, 10);
-/**/     this.dom = this.renderer.domElement;
-/**/     this.update = this.update.bind(this);
-/**/     this.resize = this.resize.bind(this);
-/**/     this.resize(w, h); // set render size
-/**/   }
-/**/   add(mesh) {
-/**/     this.scene.add(mesh);
-/**/     if (!mesh.update) return;
-/**/     this.meshListeners.push(mesh.update);
-/**/     this.meshCount++;
-/**/   }
-/**/   update() {
-/**/     let i = this.meshCount;
-/**/     while (--i >= 0) {
-/**/       this.meshListeners[i].apply(this, null);
-/**/     }
-/**/     this.renderer.render(this.scene, this.camera);
-/**/   }
-/**/   resize(w, h) {
-/**/     this.camera.aspect = w / h;
-/**/     this.camera.updateProjectionMatrix();
-/**/     this.renderer.setSize(w, h);
-/**/   }
-/**/ }
-/**/ const webgl = new Webgl(windowWidth, windowHeight);
-/**/ document.body.appendChild(webgl.dom);
-/**/
-/**/
-/* ---- CREATING ZONE ---- */
+const MAIN_COLOR = '#C9F0FF';
+const SECONDARY_COLOR = '#070707';
+const BACKGROUND_COLOR = '#ffffff';
+
+/**
+ * * *******************
+ * * CORE
+ * * *******************
+ */
+let windowWidth = window.innerWidth;
+let windowHeight = window.innerHeight;
+class Webgl {
+  constructor(w, h) {
+    this.meshCount = 0;
+    this.meshListeners = [];
+    this.renderer = new WebGLRenderer({ antialias: true, alpha: true });
+    this.renderer.setPixelRatio(window.devicePixelRatio);
+    this.renderer.setClearColor(new Color(BACKGROUND_COLOR));
+    this.scene = new Scene();
+    this.camera = new PerspectiveCamera(50, w / h, 1, 1000);
+    this.camera.position.set(0, 0, 10);
+    this.dom = this.renderer.domElement;
+    this.update = this.update.bind(this);
+    this.resize = this.resize.bind(this);
+    this.resize(w, h); // set render size
+  }
+  add(mesh) {
+    this.scene.add(mesh);
+    if (!mesh.update) return;
+    this.meshListeners.push(mesh.update);
+    this.meshCount++;
+  }
+  update() {
+    let i = this.meshCount;
+    while (--i >= 0) {
+      this.meshListeners[i].apply(this, null);
+    }
+    this.renderer.render(this.scene, this.camera);
+  }
+  resize(w, h) {
+    this.camera.aspect = w / h;
+    this.camera.updateProjectionMatrix();
+    this.renderer.setSize(w, h);
+  }
+}
+const webgl = new Webgl(windowWidth, windowHeight);
+document.body.appendChild(webgl.dom);
+
+/**
+ * * *******************
+ * * CREATING ZONE
+ * * *******************
+ */
 
 // OBJECTS
 class Example extends Object3D {
@@ -55,7 +63,7 @@ class Example extends Object3D {
     super();
 
     this.material = new MeshBasicMaterial({
-      color: new Color(secondaryColor),
+      color: new Color(MAIN_COLOR),
       shading: FlatShading,
       wireframe: true,
     });
@@ -79,20 +87,21 @@ const ex = new Example();
 // ADDS
 webgl.add(ex);
 
-/* ---- CREATING ZONE END ---- */
-/**/
-/**/
-/**/ /* ---- ON RESIZE ---- */
-/**/ function onResize() {
-/**/   windowWidth = window.innerWidth;
-/**/   windowHeight = window.innerHeight;
-/**/   webgl.resize(windowWidth, windowHeight);
-/**/ }
-/**/ window.addEventListener('resize', onResize);
-/**/ window.addEventListener('orientationchange', onResize);
-/**/ /* ---- LOOP ---- */
-/**/ function _loop() {
-/**/ 	webgl.update();
-/**/ 	requestAnimationFrame(_loop);
-/**/ }
-/**/ _loop();
+/**
+ * * *******************
+ * * RESIZE && LOOP
+ * * *******************
+ */
+function onResize() {
+  windowWidth = window.innerWidth;
+  windowHeight = window.innerHeight;
+  webgl.resize(windowWidth, windowHeight);
+}
+window.addEventListener('resize', onResize);
+window.addEventListener('orientationchange', onResize);
+// LOOP
+function loop() {
+  webgl.update();
+  requestAnimationFrame(loop);
+}
+loop();
