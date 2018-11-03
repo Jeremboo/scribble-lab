ffdc5e6f95cf6a43d8e218a062b6f9ffbd43f0c2
Jérémie Boulay <jeremi.boulay@gmail.com>
2018-11-03 17:40:04 -0400

    :gem: floating Items

---
+++ scribbles/portfolio/_modules/FloatingCube.js
@@ -0,0 +1,80 @@
+import {
+  BoxGeometry, MeshToonMaterial, Mesh, Color, Vector3,
+} from 'three';
+
+import { getRandomFloat, radians } from 'utils';
+
+/**
+ * Update current to attract him to the target using gravity and velocity
+ * Elastic effect can be made
+ * @param {Vector3 || Euler} current
+ * @param {Vector3} target
+ * @param {Number} gravity
+ * @param {Number} velocity
+ */
+function applyAttraction(current, target, gravity, velocity) {
+  const force = new Vector3();
+  return () => {
+    // const vecForce = current.clone().sub(target);
+    // force.sub(vecForce.multiplyScalar(gravity));
+    force.x -= ((current.x - target.x) * gravity);
+    force.y -= ((current.y - target.y) * gravity);
+    force.z -= ((current.z - target.z) * gravity);
+    // current.add(force);
+    current.x += force.x;
+    current.y += force.y;
+    current.z += force.z;
+    force.multiplyScalar(velocity);
+  };
+}
+
+/**
+ * * *******************
+ * * FLOATING CUBE
+ * * *******************
+ */
+export default class FloatingCube extends Mesh {
+  constructor(x, y, { force = 0.004, scale = getRandomFloat(0.5, 2), color = '#C9F0FF' }) {
+    // Create object
+    const material = new MeshToonMaterial({
+      color: new Color(color),
+    });
+    const geometry = new BoxGeometry(scale, scale, scale);
+    super(geometry, material);
+    this.castShadow = true;
+    this.receiveShadow = false;
+
+    // init
+    // this.position.set(x, y, -1);
+    this.position.set(x, y, getRandomFloat(-4, -1));
+    this.rotation.set(
+      getRandomFloat(radians(-30), radians(30)),
+      getRandomFloat(radians(-30), radians(30)),
+      0,
+    );
+
+    // Animation
+    // this.targetedPosition = new Vector3(x, y, scale * -0.25);
+    this.targetedPosition = new Vector3(x, y, -((scale * 0.5) - 0.5));
+    this.targetedRotation = new Vector3();
+
+    this.attractPosition = applyAttraction(this.position, this.targetedPosition, force, 0.92);
+    this.attractRotation = applyAttraction(this.rotation, this.targetedRotation, force * 2, 0.96);
+
+    setTimeout(() => {
+      this.targetedPosition.set(x, y, -scale);
+      this.targetedRotation.set(
+        getRandomFloat(radians(-30), radians(30)),
+        getRandomFloat(radians(-30), radians(30)),
+        0,
+      );
+    }, getRandomFloat(4000, 4500));
+
+    this.update = this.update.bind(this);
+  }
+
+  update() {
+    this.attractPosition();
+    this.attractRotation();
+  }
+}
---
+++ scribbles/portfolio/floatingItems/app.js
@@ -0,0 +1,200 @@
+import {
+  WebGLRenderer, Scene, PerspectiveCamera, BoxGeometry,
+  MeshToonMaterial, Mesh, Color, PlaneGeometry, MeshBasicMaterial,
+  Vector3, AmbientLight, DirectionalLight, PCFSoftShadowMap, ShadowMaterial,
+  OrthographicCamera,
+} from 'three';
+
+import FloatingCube from 'FloatingCube';
+
+import { getRandomFloat, radians } from 'utils';
+
+
+const MAIN_COLOR = '#C9F0FF';
+const SECONDARY_COLOR = '#070707';
+const BACKGROUND_COLOR = '#ffffff';
+
+/**
+ * * *******************
+ * * CORE
+ * * *******************
+ */
+let windowWidth = window.innerWidth;
+let windowHeight = window.innerHeight;
+class Webgl {
+  constructor(w, h) {
+    this.meshCount = 0;
+    this.meshListeners = [];
+    this.renderer = new WebGLRenderer({ antialias: true, alpha: true });
+    this.renderer.setPixelRatio(window.devicePixelRatio);
+    this.renderer.setClearColor(new Color(BACKGROUND_COLOR));
+    this.renderer.shadowMap.enabled = true;
+    this.renderer.shadowMap.type = PCFSoftShadowMap; // default THREE.PCFShadowMap
+    this.scene = new Scene();
+
+    // this.camera = new PerspectiveCamera(50, w / h, 1, 1000);
+    // this.camera.position.set(0, 0, 10);
+    this.camera = new OrthographicCamera(-5 * (w / h), 5 * (w / h), 5, -5, 1, 1000);
+    this.camera.position.set(0, 0, 10);
+
+    this.dom = this.renderer.domElement;
+    this.update = this.update.bind(this);
+    this.resize = this.resize.bind(this);
+    this.resize(w, h); // set render size
+  }
+  add(mesh) {
+    this.scene.add(mesh);
+    if (mesh.update) this.addLoop(mesh.update);
+  }
+  addLoop(update) {
+    this.meshListeners.push(update);
+    this.meshCount++;
+  }
+  update() {
+    let i = this.meshCount;
+    while (--i >= 0) {
+      this.meshListeners[i].apply(this, null);
+    }
+    this.renderer.render(this.scene, this.camera);
+  }
+  resize(w, h) {
+    this.camera.aspect = w / h;
+    this.camera.updateProjectionMatrix();
+    this.renderer.setSize(w, h);
+  }
+}
+const webgl = new Webgl(windowWidth, windowHeight);
+document.body.appendChild(webgl.dom);
+
+/**
+ * * *******************
+ * * ENVIRONMENT
+ * * *******************
+ */
+
+// Plane
+const planeShadowMaterial = new ShadowMaterial({
+  color: new Color(SECONDARY_COLOR),
+});
+planeShadowMaterial.opacity = 0.05;
+
+const planeShadow = new Mesh(new PlaneGeometry(10, 8, 1), planeShadowMaterial);
+planeShadow.receiveShadow = true;
+webgl.add(planeShadow);
+
+const plane = new Mesh(
+  new PlaneGeometry(12, 10, 1),
+  new MeshBasicMaterial({
+    color: BACKGROUND_COLOR,
+  }),
+);
+webgl.add(plane);
+
+// Lights
+const ambiantLight = new AmbientLight(0xffffff, 0.8);
+webgl.add(ambiantLight);
+const directionalLight = new DirectionalLight(0xffffff, 0.2);
+directionalLight.position.set(-2, 1, 2);
+directionalLight.castShadow = true;
+directionalLight.shadow.mapSize.width = 2048;
+directionalLight.shadow.mapSize.height = 2048;
+directionalLight.shadow.camera.near = 0.5;
+directionalLight.shadow.camera.far = 50;
+webgl.add(directionalLight);
+// const pointLight = new PointLight(0xfff7d7, 0.05);
+// pointLight.position.set(-2, 1, 1);
+// webgl.add(pointLight);
+
+/**
+ * * *******************
+ * * CREATING ZONE
+ * * *******************
+ */
+
+const MARGIN = 0.15;
+const RECURCIVE_RANDOM = 0.3;
+
+const LEFT  = 'left';
+const RIGHT = 'right';
+const UP    = 'top';
+const DOWN  = 'bottom';
+
+// Floating object
+function createFloatingCube(x = 0, y = 0, props) {
+  const floatingObj = new FloatingCube(x, y, Object.assign({ color: MAIN_COLOR }, props));
+  webgl.add(floatingObj);
+}
+
+// V1 --------------------------------------------------------------------------------------------------------------------
+// webgl.addLoop(() => {
+// if (Math.random() > 0.95) {
+//   createFloatingCube(
+//     getRandomFloat(-4, 4),
+//     getRandomFloat(-3, 3),
+//   );
+// }
+// });
+
+// V2 --------------------------------------------------------------------------------------------------------------------
+// function createCubeGroup(x, y, scale, forbidenFaces = []) {
+//   // create the current cube
+//   createFloatingCube(x, y, { scale, force: 0.003 });
+
+//   setTimeout(() => {
+//     // Compute the new sizes
+//     const newScale = scale * 0.4;
+//     const border = (scale * 0.5) - (newScale * 0.5);
+//     const pos = (scale * 0.5) + (newScale * 0.5) + MARGIN;
+
+//     if (newScale > 0.1) {
+//       // LEFT
+//       if (Math.random() > RECURCIVE_RANDOM && forbidenFaces.indexOf(LEFT) === -1) {
+//         (Math.random() > 0.5)
+//           ? createCubeGroup(x - pos, y + border, newScale, [...forbidenFaces, RIGHT]) // Left - Up
+//           : createCubeGroup(x - pos, y - border, newScale, [...forbidenFaces, RIGHT]) // Left - Down
+//         ;
+//       }
+//       // RIGTH
+//       if (Math.random() > RECURCIVE_RANDOM && forbidenFaces.indexOf(RIGHT) === -1) {
+//         (Math.random() > 0.5)
+//           ? createCubeGroup(x + pos, y + border, newScale, [...forbidenFaces, LEFT]) // Right - Up
+//           : createCubeGroup(x + pos, y - border, newScale, [...forbidenFaces, LEFT]) // Right - Down
+//         ;
+//       }
+//       // UP
+//       if (Math.random() > RECURCIVE_RANDOM && forbidenFaces.indexOf(UP) === -1) {
+//         (Math.random() > 0.5)
+//           ? createCubeGroup(x - border, y + pos, newScale, [...forbidenFaces, DOWN]) // Up - Left
+//           : createCubeGroup(x + border, y + pos, newScale, [...forbidenFaces, DOWN]) // Up - Right
+//         ;
+//       }
+//       // DOWN
+//       if (Math.random() > RECURCIVE_RANDOM && forbidenFaces.indexOf(DOWN) === -1) {
+//         (Math.random() > 0.5)
+//           ? createCubeGroup(x - border, y - pos, newScale, [...forbidenFaces, UP]) // Down - Left
+//           : createCubeGroup(x + border, y - pos, newScale, [...forbidenFaces, UP]) // Down - Right
+//         ;
+//       }
+//     }
+//   }, 200);
+// }
+// createCubeGroup(0, 0, 3.5);
+
+/**
+ * * *******************
+ * * RESIZE && LOOP
+ * * *******************
+ */
+function onResize() {
+  windowWidth = window.innerWidth;
+  windowHeight = window.innerHeight;
+  webgl.resize(windowWidth, windowHeight);
+}
+window.addEventListener('resize', onResize);
+window.addEventListener('orientationchange', onResize);
+// LOOP
+function loop() {
+  webgl.update();
+  requestAnimationFrame(loop);
+}
+loop();
