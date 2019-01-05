eafbf2b13ea000ed044dc2d309ef02e224e5fd12
Jérémie Boulay <jeremi.boulay@gmail.com>
2019-01-04 23:45:22 -0500

    :art: multiple updates

--- modules/AnimatedText3D.js
+++ modules/AnimatedText3D.js
@@ -7,7 +7,7 @@ const fontLoader = new FontLoader();
 const font = fontLoader.parse(fontFile);
 
 export default class AnimatedText3D extends Object3D {
-  constructor(text, { size = 0.3, letterSpacing = 0.03, color = '#000000' } = {}) {
+  constructor(text, { size = 0.3, letterSpacing = 0.03, color = '#000000', duration = 0.6 } = {}) {
     super();
 
     this.basePosition = 0;
@@ -35,21 +35,32 @@ export default class AnimatedText3D extends Object3D {
         this.add(mesh);
       }
     });
-  }
-  show(duration = 0.6) {
-    const tm = new TimelineLite();
-    tm.set({}, {}, `+=${duration * 1.1}`)
+
+    this.show = this.show.bind(this);
+    this.hide = this.hide.bind(this);
+
+    // Timeline
+    this.tm = new TimelineLite({ paused: true });
+    this.tm.set({}, {}, `+=${duration * 1.1}`)
     this.children.forEach((letter) => {
       const data = {
         opacity: 0,
         position: -0.5,
       };
-      tm.to(data, duration, { opacity: 1, position: 0, ease: Back.easeOut.config(2), onUpdate: () => {
+      this.tm.to(data, duration, { opacity: 1, position: 0, ease: Back.easeOut.config(2), onUpdate: () => {
         letter.material.opacity = data.opacity;
         letter.position.y = data.position;
         letter.position.z = data.position * 2;
         letter.rotation.x = data.position * 2;
       } }, `-=${duration - 0.03}`);
     });
+  }
+
+  show() {
+    this.tm.play();
+  }
+
+  hide() {
+    this.tm.reverse();
   }
 }
