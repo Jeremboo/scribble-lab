d0d85382a56f7e849218dfe9ede93216fa997974
Jérémie Boulay <jeremi.boulay@gmail.com>
2020-06-21 17:55:41 -0400

    Create Mesh and Renderer

---
+++ modules/WebGL/Mesh.js
@@ -0,0 +1,76 @@
+import Program from "./Program";
+import Object3D from "./Object3D";
+
+export const DEFAULT_VERTEX = `
+  varying vec2 vUv;
+
+  void main() {
+    gl_Position = viewProjectionMatrix * modelMatrix * position;
+    vUv = position.xy + 0.5;
+  }
+`;
+export const DEFAULT_FRAGMENT = `
+  precision mediump float;
+
+  varying vec2 vUv;
+
+  void main() {
+    gl_FragColor = vec4(vUv, 1.0, 1.0);
+  }
+`;
+export const DEFAULT_PLANE_POSITION = [
+  -0.5, 0.5, 0,
+  -0.5, -0.5, 0,
+  0.5, -0.5, 0,
+  -0.5, 0.5, 0,
+  0.5, 0.5, 0,
+  0.5, -0.5, 0,
+];
+
+export default class Mesh extends Object3D {
+  constructor(context, camera, {
+    vertex = DEFAULT_VERTEX,
+    fragment = DEFAULT_FRAGMENT,
+    position = DEFAULT_PLANE_POSITION,
+    positionSize = 3,
+    uniforms = {}
+  } = {}) {
+    super();
+
+    const transformedVextrex = `
+      uniform mat4 modelMatrix;
+      uniform mat4 viewProjectionMatrix;
+      attribute vec4 position;
+    ` + vertex;
+
+    this.camera = camera;
+    this.program = new Program(context, transformedVextrex, fragment);
+
+    // Add the default position
+    this.program.addAttributePosition(position, positionSize);
+    // Add the uniforms and matrix for positionning
+    this.program.addUniforms(Object.assign({
+      viewProjectionMatrix: this.camera.getViewProjectionMatrix(),
+      modelMatrix: this._matrix,
+    }, uniforms));
+  }
+
+  /**
+   * Overwrite the matrix setter to update
+   * the modelMatrix uniform each time it's updated.
+   */
+  set matrix(mat4) {
+    this._matrix = mat4;
+    this.program.forceUpdateUniform('modelMatrix', this._matrix);
+  }
+
+  /**
+   * * *******************
+   * * CAMERA
+   * * *******************
+   */
+  setCamera(camera) {
+    this.camera = camera;
+    this.program.forceUpdateUniform('viewProjectionMatrix', this.camera.getViewProjectionMatrix());
+  }
+}
---
+++ modules/WebGL/Renderer.js
@@ -0,0 +1,47 @@
+
+/**
+ * Used to loop the Program in the most performant way
+ */
+export default class Renderer {
+  constructor(context) {
+    this.gl = context;
+    this.i = 0;
+    this.programs = [];
+    this.programCount = -1;
+    this.programLastUsed = false;
+
+    this.render = this.render.bind(this);
+  }
+
+  /**
+   * * *******************
+   * * PROGRAM
+   * * *******************
+   */
+
+  addProgram(program) {
+    this.programCount++;
+    this.programs.unshift(program);
+  }
+  removeProgram(program) {
+    // TODO 2020-05-31 jeremboo:
+  }
+
+  /**
+   * * *******************
+   * * RENDER
+   * * *******************
+   */
+
+  // TODO 2020-06-18 jeremboo: Improve it
+  // TODO 2020-06-21 jeremboo: pass the camera as a props there?
+  render() {
+    for(this.i = this.programCount; this.i >= 0; this.i--) {
+      if (this.programLastUsed !== this.programs[this.i].id) {
+        this.programs[this.i].useProgram();
+        this.programLastUsed = this.programs[this.i].id;
+      }
+      this.gl.drawArrays(this.gl.TRIANGLES, 0, this.programs[this.i].count);
+    }
+  }
+}
